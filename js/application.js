0b9248377e8522b4df361839153ac31f95f79f40
Thomasbka <tolle_bka@hotmail.com>
2024-04-26 09:54:15 +0200

    remove button funciton added

--- js/application.js
+++ js/application.js
@@ -1,16 +1,36 @@
-var updateShoppingCartValue = function () {
-  var price = [];
-  var quantity = [];
-
+var updateSubTotalPrice = function (ele) {
+  var price = parseFloat($(ele).find('.price input').val());
+  var quantity = parseFloat($(ele).find('.quantity input').val());
   
+  if (isNaN(price) || isNaN(quantity)) {
+    console.error('Invalid input for price or quantity');
+    return;
+  }
+
+  var subTotalPrice = price * quantity;
+  $(ele).children('.totalItemPrice').html(totalItemPrice.toFixed(2));
+
+  return subTotalPrice;
+}
+
+var updateShoppingCartValue = function () {
+  var subTotalPrice = [];
 }
 
 $(document).ready(function () {
-  updateShoppingCartValue;
+  updateShoppingCartValue();
 
-  $(document).on('click', 'btn.remove', function (event) {
+  $(document).on('click', '.btn.remove', function (event) {
     $(this).closest('tr').remove();
     updateShoppingCartValue();
+  });
+
+  var timeout;
+  $(document).on('input', 'tr input', function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      updateShoppingCartValue();
+    }, 1000);
   })
 
 });
